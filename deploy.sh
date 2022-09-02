51e94ca4f21f681087f11ca1d9f56c508c2eb33c
szabikr <szabikr.dev@gmail.com>
2022-09-02 14:39:57 +0300

    Implements deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+if [ -z ${HBT_APP_ROOT_DIR+x} ]; then
+	echo "Habit Tracker App root directory (\$HBT_APP_ROOT_DIR) should be set before deploying."
+	exit 1
+fi
+
+echo "deployment can start"
+
+echo "Creating app root directory..."
+mkdir $HBT_APP_ROOT_DIR
+echo "Creating low level module directories..."
+mkdir $HBT_APP_ROOT_DIR/activities
+echo "Creating user_input directory..."
+mkdir $HBT_APP_ROOT_DIR/user_input
+
+echo "Creating data.txt file..."
+touch $HBT_APP_ROOT_DIR/data.txt
+echo "Creating logs.txt file..."
+touch $HBT_APP_ROOT_DIR/logs.txt
+
+cp activities/* $HBT_APP_ROOT_DIR/activities
+cp import_activities.py when_did_i_do.py $HBT_APP_ROOT_DIR
+cp log_config.yaml logging_config.py $HBT_APP_ROOT_DIR
+cp main.py $HBT_APP_ROOT_DIR
+
+cp requirements.txt $HBT_APP_ROOT_DIR
