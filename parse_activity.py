ad5e65c7035629283fa75bcbcd40028297410919
szabikr <szabikr.dev@gmail.com>
2022-10-31 10:33:35 +0200

    Makes parsing activity and journal entry even simpler

--- parse_activity.py
+++ parse_activity.py
@@ -1,37 +1,35 @@
-import logging
-from datetime import date
+from collections import namedtuple
 
-from activities.activity import Activity
-from activities.guess_life_aspect import guess_life_aspect
+ParsedActivityFields = ["activity_name", "life_aspect", "more_info"]
+ParsedActivity = namedtuple("ParsedActivity", ParsedActivityFields)
 
-logger = logging.getLogger(__name__)
-
-def parse_activity(line: str, activity_date: date) -> Activity:
+def parse_activity(line: str) -> ParsedActivity:
     activity_parts = line.split("|")
     raw_activity_props = activity_parts[0]
     try:
         more_info = activity_parts[1].strip()
     except IndexError:
         more_info = None
+    
     activity_props = raw_activity_props.split(";")
     activity_name = activity_props[0].strip()
     try:
         life_aspect = activity_props[1].strip()
     except IndexError:
-        life_aspect = guess_life_aspect(activity_name)
-        if not life_aspect:
-            logger.exception(f"'{line}' is not a correct activity description")
-            return None
-    return Activity(activity_name, activity_date, life_aspect, more_info)
+        life_aspect = None
+    
+    return ParsedActivity(activity_name, life_aspect, more_info)
 
 if __name__ == "__main__":
-    activity_date = date.today()
     activity_line1 = "developing my habit tracker; Career | Working on the more info feature"
-    activity1 = parse_activity(activity_line1, activity_date)
-    print(activity1.print())
+    activity1 = parse_activity(activity_line1)
+    print(f"{activity1.activity_name};{activity1.life_aspect};{activity1.more_info}")
     activity_line2 = "developing my habit tracker; Career"
-    activity2 = parse_activity(activity_line2, activity_date)
-    print(activity2.print())
+    activity2 = parse_activity(activity_line2)
+    print(f"{activity2.activity_name};{activity2.life_aspect};{activity2.more_info}")
     activity_line3 = "developing my habit tracker | Working on the more info feature"
-    activity3 = parse_activity(activity_line3, activity_date)
-    print(activity3.print())
+    activity3 = parse_activity(activity_line3)
+    print(f"{activity3.activity_name};{activity3.life_aspect};{activity3.more_info}")
+    activity_line4 = "developing my habit tracker"
+    activity4 = parse_activity(activity_line4)
+    print(f"{activity4.activity_name};{activity4.life_aspect};{activity4.more_info}")
--- parse_journal_entry.py
+++ parse_journal_entry.py
@@ -1,29 +1,15 @@
 from typing import List
-from datetime import date
 
-from journal.journal_entry import JournalEntry
-
-def parse_journal_entry(lines: List[str], record_date: date) -> JournalEntry:
-    record = ""
-    for line in lines:
-        if record != "":
-            raw_new_line = r"\n"
-            record = f"{record}{raw_new_line}"
-        record = f"{record}{line}"
-
-    if record == "": 
-        return None
-
-    return JournalEntry(record, record_date)
+def parse_journal_entry(lines: List[str]) -> str:
+    return r"\n".join(lines)
 
 if __name__ == "__main__":
-    journal_entry_date = date.today()
     journal_lines1 = ["first line", "then the second line", "finally the third one"]
-    journal_entry1 = parse_journal_entry(journal_lines1, journal_entry_date)
-    print(journal_entry1.print())
+    journal_entry1 = parse_journal_entry(journal_lines1)
+    print(journal_entry1)
     journal_lines2 = ["first line"]
-    journal_entry2 = parse_journal_entry(journal_lines2, journal_entry_date)
-    print(journal_entry2.print())
+    journal_entry2 = parse_journal_entry(journal_lines2)
+    print(journal_entry2)
     journal_lines2 = []
-    journal_entry2 = parse_journal_entry(journal_lines2, journal_entry_date)
-    print(journal_entry2 == None)
+    journal_entry2 = parse_journal_entry(journal_lines2)
+    print(journal_entry2)
